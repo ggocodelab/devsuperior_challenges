047f30c99274326ede68b8dfaaaa335a6afdc919
ggocodelab <brgillian@gmail.com>
2025-10-01 16:40:25 -0300

    Service done

--- challenge1/src/main/java/com/ggocodelab/challenge/services/OrderService.java
+++ challenge1/src/main/java/com/ggocodelab/challenge/services/OrderService.java
@@ -2,7 +2,19 @@ package com.ggocodelab.challenge.services;
 
 import org.springframework.stereotype.Service;
 
+import com.ggocodelab.challenge.entities.Order;
+
 @Service
 public class OrderService {
-
+	
+	private ShippingService shippingService;
+	
+	public OrderService(ShippingService shippingService) {
+		this.shippingService = shippingService;
+	}
+	
+	public double total(Order order) {
+		return order.getBasic() - (order.getBasic() * order.getDiscount()/100) 
+				+ shippingService.shipment(order);
+	}
 }
--- challenge1/src/main/java/com/ggocodelab/challenge/services/ShippingService.java
+++ challenge1/src/main/java/com/ggocodelab/challenge/services/ShippingService.java
@@ -2,7 +2,24 @@ package com.ggocodelab.challenge.services;
 
 import org.springframework.stereotype.Service;
 
+import com.ggocodelab.challenge.entities.Order;
+
 @Service
 public class ShippingService {
+	
+	public double shipment(Order order) {
+		if(order.getBasic() >= 200.00) {
+			return 0.0;
+		}
+		
+		if(order.getBasic() >= 100.0 && order.getBasic() < 200.0) {
+			return 12.00;
+		}
+		
+		if(order.getBasic() < 100.0) {
+			return 20.0;
+		}
+		return shipment(order);
+	}
 
 }
